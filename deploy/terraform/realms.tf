7cb5cc50568aedeb9ab89f69dc79b13e1f0ec9ca
Craig Miller <craigmiller160@gmail.com>
2023-01-03 17:34:03 -0500

    More settings

--- deploy/terraform/realms.tf
+++ deploy/terraform/realms.tf
@@ -4,6 +4,7 @@ locals {
       verify_email = true
       reset_password_allowed = true
       registration_email_as_username = true
+      login_with_email = true
     }
   }
 }
@@ -17,6 +18,7 @@ resource "keycloak_realm" "apps_dev" {
   verify_email = local.common_realm_settings.login.verify_email
   reset_password_allowed = local.common_realm_settings.login.reset_password_allowed
   registration_email_as_username = local.common_realm_settings.login.registration_email_as_username
+  login_with_email = local.common_realm_settings.login.login_with_email
 }
 
 resource "keycloak_realm" "apps_prod" {
@@ -28,4 +30,5 @@ resource "keycloak_realm" "apps_prod" {
   verify_email = local.common_realm_settings.login.verify_email
   reset_password_allowed = local.common_realm_settings.login.reset_password_allowed
   registration_email_as_username = local.common_realm_settings.login.registration_email_as_username
+  login_with_email = local.common_realm_settings.login.login_with_email
 }
