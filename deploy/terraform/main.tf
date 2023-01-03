26841f411c37d0d77012f48d1523a0428bc220b3
Craig Miller <craigmiller160@gmail.com>
2023-01-03 16:31:04 -0500

    Figured out how to get the credentials from non-standard fields... it's ugly

--- deploy/terraform/main.tf
+++ deploy/terraform/main.tf
@@ -5,10 +5,10 @@ terraform {
   }
 
   required_providers {
-#    keycloak = {
-#      source = "mrparkers/keycloak"
-#      version = "4.1.0"
-#    }
+    keycloak = {
+      source = "mrparkers/keycloak"
+      version = "4.1.0"
+    }
 
     onepassword = {
       source = "1Password/onepassword"
@@ -17,16 +17,11 @@ terraform {
   }
 }
 
-output "my_onepassword_item" {
-  value = data.onepassword_item.terraform_client
-  sensitive = true
+provider "keycloak" {
+  client_id = data.onepassword_item.terraform_client.section[0].field[0].value
+  client_secret = data.onepassword_item.terraform_client.section[0].field[1].value
+  url = "https://auth-craigmiller160.ddns.net"
 }
-
-#provider "keycloak" {
-#  client_id = data.onepassword_item.terraform_client.section.keycloak.clientId
-#  client_secret = data.onepassword_item.terraform_client.clientSecret
-#  url = "https://auth-craigmiller160.ddns.net"
-#}
 
 provider "onepassword" {
   url = "https://infra-craigmiller160.ddns.net/onepassword"
