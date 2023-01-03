4f1190a24fe5d456a8dac03ee0eb8f666b801188
Craig Miller <craigmiller160@gmail.com>
2023-01-03 14:15:56 -0500

    Updated realms

--- deploy/terraform/realms.tf
+++ deploy/terraform/realms.tf
@@ -2,5 +2,5 @@ resource "keycloak_realm" "apps_dev" {
   realm = "apps-dev"
   enabled = true
   display_name = "Apps (Dev)"
-  display_name_html = '<div class="kc-logo-text"><span>Apps (Dev)</span></div>'
+  display_name_html = "<div class='kc-logo-text'><span>Apps (Dev)</span></div>"
 }
