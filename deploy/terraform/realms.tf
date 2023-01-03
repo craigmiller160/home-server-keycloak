750f4f4930b6e1d304c2ea339f13f9399291952d
Craig Miller <craigmiller160@gmail.com>
2023-01-03 17:28:34 -0500

    Working on realms

--- deploy/terraform/realms.tf
+++ deploy/terraform/realms.tf
@@ -1,8 +1,20 @@
+locals {
+  common_realm_settings = {
+    login = {
+      registration_email_as_username = true
+    }
+  }
+}
+
 resource "keycloak_realm" "apps_dev" {
   realm = "apps-dev"
   enabled = true
   display_name = "Apps (Dev)"
   display_name_html = "<div class='kc-logo-text'><span>Apps (Dev)</span></div>"
+
+  login = {
+    registration_email_as_username = true
+  }
 }
 
 resource "keycloak_realm" "apps_prod" {
