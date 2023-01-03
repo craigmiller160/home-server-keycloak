4344773dbec374b029645bf9679b7d581f71a94b
Craig Miller <craigmiller160@gmail.com>
2023-01-03 16:08:19 -0500

    Another attemptg

--- deploy/terraform/main.tf
+++ deploy/terraform/main.tf
@@ -17,11 +17,15 @@ terraform {
   }
 }
 
-provider "keycloak" {
-  client_id = data.onepassword_item.terraform_client.section.keycloak.clientId
-  client_secret = data.onepassword_item.terraform_client.clientSecret
-  url = "https://auth-craigmiller160.ddns.net"
+output "my_onepassword_item" {
+  value = data.onepassword_item.terraform_client
 }
+
+#provider "keycloak" {
+#  client_id = data.onepassword_item.terraform_client.section.keycloak.clientId
+#  client_secret = data.onepassword_item.terraform_client.clientSecret
+#  url = "https://auth-craigmiller160.ddns.net"
+#}
 
 provider "onepassword" {
   url = "https://infra-craigmiller160.ddns.net/onepassword"
--- deploy/terraform/realms.tf
+++ deploy/terraform/realms.tf
@@ -1,13 +1,13 @@
-resource "keycloak_realm" "apps_dev" {
-  realm = "apps-dev"
-  enabled = true
-  display_name = "Apps (Dev)"
-  display_name_html = "<div class='kc-logo-text'><span>Apps (Dev)</span></div>"
-}
-
-resource "keycloak_realm" "apps_prod" {
-  realm = "apps-prod"
-  enabled = true
-  display_name = "Apps (Prod)"
-  display_name_html = "<div class='kc-logo-text'><span>Apps (Prod)</span></div>"
-}
+#resource "keycloak_realm" "apps_dev" {
+#  realm = "apps-dev"
+#  enabled = true
+#  display_name = "Apps (Dev)"
+#  display_name_html = "<div class='kc-logo-text'><span>Apps (Dev)</span></div>"
+#}
+#
+#resource "keycloak_realm" "apps_prod" {
+#  realm = "apps-prod"
+#  enabled = true
+#  display_name = "Apps (Prod)"
+#  display_name_html = "<div class='kc-logo-text'><span>Apps (Prod)</span></div>"
+#}
