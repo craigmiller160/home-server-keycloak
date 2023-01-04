60da42974bdec0cc44ffe2f63e117815b90e9b4e
Craig Miller <craigmiller160@gmail.com>
2023-01-03 21:12:27 -0500

    Updating required actions

--- deploy/terraform/required_actions.tf
+++ deploy/terraform/required_actions.tf
@@ -1,19 +1,56 @@
-resource "keycloak_required_action" "configure_otp" {
-  realm_id = keycloak_realm.apps_prod.id
-  alias    = "CONFIGURE_TOTP"
-  enabled  = true
-  name     = "Configure OTP"
-  default_action = false
-  priority = 1
+locals {
+  required_actions = {
+    configure_otp = {
+      alias = "CONFIGURE_TOTP"
+      name = "Configure OTP"
+      enabled = true
+      default_action = false
+      priority = 1
+    }
+    update_password = {
+      alias = "UPDATE_PASSWORD"
+      name = "Update Password"
+      enabled = true
+      default_action = true
+      priority = 2
+    }
+  }
 }
 
-resource "keycloak_required_action" "update_password" {
-  realm_id = keycloak_realm.apps_prod.id
-  alias    = "UPDATE_PASSWORD"
-  enabled  = true
-  name     = "Update Password"
-  default_action = true
-  priority = 2
+resource "keycloak_required_action" "dev_configure_otp" {
+  realm_id = keycloak_realm.apps_dev.id
+  alias = local.required_actions.configure_otp.alias
+  name = local.required_actions.configure_otp.name
+  enabled = local.required_actions.configure_otp.enabled
+  default_action = local.required_actions.configure_otp.default_action
+  priority = local.required_actions.configure_otp.priority
 }
 
-#  Error: validation error: required action "update-password" does not exist on the server, installed providers: [terms_and_conditions update_user_locale delete_account webauthn-register-passwordless VERIFY_PROFILE UPDATE_PASSWORD CONFIGURE_TOTP VERIFY_EMAIL webauthn-register UPDATE_PROFILE]
+resource "keycloak_required_action" "prod_configure_otp" {
+  realm_id = keycloak_realm.apps_prod.id
+  alias = local.required_actions.configure_otp.alias
+  name = local.required_actions.configure_otp.name
+  enabled = local.required_actions.configure_otp.enabled
+  default_action = local.required_actions.configure_otp.default_action
+  priority = local.required_actions.configure_otp.priority
+}
+
+resource "keycloak_required_action" "dev_update_password" {
+  realm_id = keycloak_realm.apps_dev.id
+  alias = local.required_actions.update_password.alias
+  name = local.required_actions.update_password.name
+  enabled = local.required_actions.update_password.enabled
+  default_action = local.required_actions.update_password.default_action
+  priority = local.required_actions.update_password.priority
+}
+
+resource "keycloak_required_action" "prod_update_password" {
+  realm_id = keycloak_realm.apps_prod.id
+  alias = local.required_actions.update_password.alias
+  name = local.required_actions.update_password.name
+  enabled = local.required_actions.update_password.enabled
+  default_action = local.required_actions.update_password.default_action
+  priority = local.required_actions.update_password.priority
+}
+
+# [terms_and_conditions update_user_locale delete_account webauthn-register-passwordless VERIFY_PROFILE UPDATE_PASSWORD CONFIGURE_TOTP VERIFY_EMAIL webauthn-register UPDATE_PROFILE]
