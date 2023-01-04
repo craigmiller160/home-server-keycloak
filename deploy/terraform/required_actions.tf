0ca1d7dc2f67b729a952a4613552d790feb694c2
Craig Miller <craigmiller160@gmail.com>
2023-01-03 21:07:55 -0500

    Tweaked action

--- deploy/terraform/required_actions.tf
+++ deploy/terraform/required_actions.tf
@@ -4,6 +4,7 @@ resource "keycloak_required_action" "required_action" {
   enabled  = true
   name     = "Update Password"
   default_action = true
+  priority = 2
 }
 
 #  Error: validation error: required action "update-password" does not exist on the server, installed providers: [terms_and_conditions update_user_locale delete_account webauthn-register-passwordless VERIFY_PROFILE UPDATE_PASSWORD CONFIGURE_TOTP VERIFY_EMAIL webauthn-register UPDATE_PROFILE]
