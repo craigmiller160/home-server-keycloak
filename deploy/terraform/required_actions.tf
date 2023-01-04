c161775272ee03981b8e83f2ebde537c639a05a6
Craig Miller <craigmiller160@gmail.com>
2023-01-03 21:07:13 -0500

    Trying to make it work

--- deploy/terraform/required_actions.tf
+++ deploy/terraform/required_actions.tf
@@ -1,6 +1,6 @@
 resource "keycloak_required_action" "required_action" {
   realm_id = keycloak_realm.apps_prod.id
-  alias    = "update-password"
+  alias    = "UPDATE_PASSWORD"
   enabled  = true
   name     = "Update Password"
   default_action = true
