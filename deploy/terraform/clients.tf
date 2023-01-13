c5a2fa9a1855704dd7930fdcd89694458bc35219
Craig Miller <craigmiller160@gmail.com>
2023-01-13 13:56:02 -0500

    Added postman clients

---
+++ deploy/terraform/clients.tf
@@ -0,0 +1,27 @@
+locals {
+  client_common = {
+    client_id = "postman"
+    name = "postman"
+    enabled = true
+    access_type = "CONFIDENTIAL"
+    service_accounts_enabled = true
+  }
+}
+
+resource "keycloak_openid_client" "postman_dev" {
+  realm_id = keycloak_realm.apps_dev.id
+  client_id = local.client_common.client_id
+  name = local.client_common.name
+  enabled = local.client_common.enabled
+  access_type = local.client_common.access_type
+  service_accounts_enabled = local.client_common.service_accounts_enabled
+}
+
+resource "keycloak_openid_client" "postman_prod" {
+  realm_id = keycloak_realm.apps_prod.id
+  client_id = local.client_common.client_id
+  name = local.client_common.name
+  enabled = local.client_common.enabled
+  access_type = local.client_common.access_type
+  service_accounts_enabled = local.client_common.service_accounts_enabled
+}
